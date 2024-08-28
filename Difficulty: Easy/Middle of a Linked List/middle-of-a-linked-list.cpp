9b5f05ccac16e70a487a3d7a97e7c3124073cc59
Prachi Singh <142558100+Prachi309@users.noreply.github.com>
2024-08-28 19:51:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.cpp
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.cpp
@@ -1,61 +1,59 @@
 //{ Driver Code Starts
-//Initial template for C++
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-struct Node
-{
+// Initial template for C++
+
+struct Node {
     int data;
     struct Node* next;
-    
-    Node(int x){
+
+    Node(int x) {
         data = x;
         next = NULL;
     }
 };
-void printList(Node* node) 
-{ 
-    while (node != NULL) { 
-        cout << node->data <<" "; 
-        node = node->next; 
-    }  
-    cout<<"\n";
-} 
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
 
 
 // } Driver Code Ends
-/* Link list Node 
+/* Link list Node
 struct Node {
     int data;
     Node* next;
-    
+
     Node(int x){
         data = x;
         next = NULL;
     }
-    
+
 }; */
-class Solution{
-    public:
-    /* Should return data of middle node. If linked list is empty, then  -1*/
-    int getMiddle(Node *head)
-    {
-        // Your code here
-        if(head == NULL) {
-            return -1;
+class Solution {
+  public:
+    /* Should return data of middle node. If linked list is empty, then -1 */
+    int getMiddle(Node* head) {
+        
+        if(head==NULL) return -1;
+        
+        // code here
+        Node * fast=head ;
+        Node* slow= head;
+        
+        while(fast!=NULL && fast->next !=NULL){
+            slow= slow->next;
+            fast= fast->next->next;
         }
-        
-        Node* slow = head;
-        Node* fast = head;
-        
-        while(fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        
         return slow->data;
-    
     }
 };
 
@@ -63,26 +61,39 @@ class Solution{
 //{ Driver Code Starts.
 
 int main() {
-    //code
+
     int t;
-    cin>>t;
-    while(t--){
-        int N;
-        cin>>N;
-        int data;
-        cin>>data;
-        struct Node *head = new Node(data);
-        struct Node *tail = head;
-        for (int i = 0; i < N-1; ++i)
-        {
-            cin>>data;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after reading t
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
             tail->next = new Node(data);
             tail = tail->next;
         }
-        
+
         Solution ob;
         cout << ob.getMiddle(head) << endl;
     }
+
     return 0;
 }
 
