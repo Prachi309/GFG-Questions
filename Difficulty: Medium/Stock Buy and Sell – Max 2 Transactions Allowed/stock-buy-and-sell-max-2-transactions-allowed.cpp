d7b1bc9e81abc3d90687183906f2f720ccac0894
Prachi Singh <142558100+Prachi309@users.noreply.github.com>
2024-09-18 17:53:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock Buy and Sell – Max 2 Transactions Allowed/stock-buy-and-sell-max-2-transactions-allowed.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+        //User function Template for C++
+        
+        int maxProfit(vector<int>&prices){
+            //Write your code here..
+             int n= prices.size();
+        vector<vector<int>>after(2, vector<int>(3,0));
+        vector<vector<int>>curr(2, vector<int>(3,0));
+        
+        for(int ind= n-1;ind >=0;ind--){
+            for(int buy = 0 ;buy <=1; buy++){
+                for(int cap =1 ; cap<=2;cap++){
+                    if(buy){
+                        curr[buy][cap] = max(-prices[ind]+ after[0][cap], 0+ after[1][cap]);
+                    }
+                    else{
+                        curr[buy][cap]= max(prices[ind]+ after[1][cap-1], 0+ after[0][cap]);
+                    }
+                }
+                after= curr;
+            }
+        }
+        return after[1][2];
+        }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        vector<int> price(n);
+        for(int i=0;i<n;i++) cin>>price[i];
+        Solution obj;
+        cout<<obj.maxProfit(price)<<endl;
+    }
+
+}
+
+// } Driver Code Ends
