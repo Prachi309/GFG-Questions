b56a554a64a82f543ff5bfe55f8c64c5395056f0
Prachi Singh <142558100+Prachi309@users.noreply.github.com>
2024-10-27 12:06:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Jump Game/jump-game.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int A[], int N) {
+        // code here
+        int maxind=0;
+        for(int i=0;i<N;i++){
+            if(i>maxind) return 0;
+            
+            maxind=max(maxind, i+A[i]);
+        }
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
