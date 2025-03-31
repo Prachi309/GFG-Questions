28d100488c94975312fd10bc0f364b35c22a5aca
Prachi Singh <142558100+Prachi309@users.noreply.github.com>
2025-03-31 09:30:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -3,42 +3,57 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to find the number of islands.
-     void dfs(int i,int j, vector<vector<char>>& grid, vector<vector<int>>& vis,int n, int m) {
-        vis[i][j] = 1;
-        
-        for(int delrow=-1;delrow<=1;delrow++){
-            for(int delcol=-1;delcol<=1;delcol++){
-                int nrow= i+delrow;
-                int ncol= j+delcol;
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
-                    dfs(nrow,ncol, grid,vis,n,m);
+    
+    void bfs(int i, int j, vector<vector<char>>& grid, 
+    vector<vector<int>>&vis){
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        int n = grid.size(), m= grid[0].size();
+        while(!q.empty()){
+            auto it = q.front();
+            int row= it.first;
+            int col= it.second;
+            q.pop();
+            
+            for(int r= -1 ; r<=1; r++ ){
+                for(int c=-1;c<=1;c++){
+                    int nrow= row+r;
+                    int ncol = col+c;
+                    if(nrow>=0 && nrow<n &&ncol>=0 && ncol<m && !vis[nrow][ncol]
+                    && grid[nrow][ncol]=='1'){
+                        vis[nrow][ncol]=1;
+                        q.push({nrow,ncol});
+                    }
+                    
+                    
                 }
             }
         }
+        
     }
     int numIslands(vector<vector<char>>& grid) {
         // Code here
         int n= grid.size();
         int m= grid[0].size();
-       int count=0;
-        vector<vector<int>>vis(n,vector<int>(m,0)) ;
-        
-        for (int i = 0; i < n; i++) {
-            for(int j=0;j<m;j++){
-            if (!vis[i][j]&& grid[i][j]=='1') {
-                count++;
-                dfs(i,j, grid, vis,n,m);
-            }
-            }
-        }
-        
-        return count;
-        
+       vector<vector<int>>vis(n, vector<int>(m,0));
+       int cnt=0;
+       
+       for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+               if(!vis[i][j] && grid[i][j]=='1'){
+                   cnt++;
+                   bfs(i,j,grid,vis);
+               }
+           }
+       }
+       return cnt;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
