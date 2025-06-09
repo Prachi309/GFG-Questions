cc374ad66fdcdbe8a988ca04bc63d47c422cdda1
Prachi Singh <142558100+Prachi309@users.noreply.github.com>
2025-06-09 16:48:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/BST with Dead End/bst-with-dead-end.cpp
@@ -0,0 +1,30 @@
+/*The Node structure is
+class Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+  bool solve(Node* node, int mini, int maxi){
+        if(!node) return false;
+        if(!node->left && !node->right){
+            return mini==maxi;
+        }
+        return solve(node->left, mini, node->data-1)||
+        solve(node->right, node->data+1, maxi);
+    }
+  
+    bool isDeadEnd(Node *root) {
+        // Code here
+         return solve(root, 1, 1e9);
+    }
+};
